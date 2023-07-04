fbaa08bbfe1d22babbbfcbcefa5440f6888d901f
Gerald <adudagerald@gmail.com>
2023-07-04 13:21:16 +0300

    only admin allowed to add and delete food, food can only be deleted by admin who added food, reviews show username, get all users does not show password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_03_122628) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_04_100003) do
   create_table "foods", force: :cascade do |t|
     t.string "name"
     t.string "image"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_03_122628) do
     t.string "restaurant_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
